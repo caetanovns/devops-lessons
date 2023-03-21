f08f303c1a42fb0dc8c2c16f570cc14b04d65c61
caetanovns <caetanov120@gmail.com>
2023-03-21 11:28:13 -0300

    - Adicinado docstring

--- lesson_3/my_app/src/calc.py
+++ lesson_3/my_app/src/calc.py
@@ -1,13 +1,24 @@
+
+''' Pacote responsável pela calculadora '''
+
+
 class Calc():
+    """Class da Calculadora"""
 
-    def sum(self, a, b):
-        return a + b
+    def sum(self, input_one, input_two):
+        '''Soma 2 números'''
+        return input_one + input_two
 
-    def sub(self, a, b):
-        return a - b
+    def sub(self, input_one, input_two):
+        '''Subtrai 2 números'''
+        return input_one - input_two
 
-    def multi(self, a, b):
-        return a * b
 
-    def divisoin(self, a, b):
-        return a / b
+    def multi(self, input_one, input_two):
+        '''Multiplica 2 números'''
+        return input_one * input_two
+
+
+    def divisoin(self, input_one, input_two):
+        '''Divide 2 números'''
+        return input_one / input_two

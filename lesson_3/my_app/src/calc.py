e3ac346fdacbfdaaea1ec874de60c2c10c2361a9
caetanovns <caetanov120@gmail.com>
2023-03-23 19:30:26 -0300

    resolvido o meu problema

--- lesson_3/my_app/src/calc.py
+++ lesson_3/my_app/src/calc.py
@@ -7,7 +7,7 @@ class Calc():
 
     def sum(self, input_one, input_two):
         '''Soma 2 números'''
-        return input_one + input_two
+        return input_one + input_two + 2
 
     def sub(self, input_one, input_two):
         '''Subtrai 2 números'''

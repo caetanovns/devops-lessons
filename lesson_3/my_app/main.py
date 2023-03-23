daa3cc7bbb1e66c36d4062f21e0c7a616d638338
caetanovns <caetanov120@gmail.com>
2023-03-23 18:41:52 -0300

    - Removido arquivos de person

--- lesson_3/my_app/main.py
+++
@@ -1,5 +0,0 @@
-from src.person import Person
-
-p = Person("Caetano", "Segundo", "caetano.segundo@fapce.edu.br")
-
-p.toString()
--- lesson_3/my_app/src/person.py
+++
@@ -1,9 +0,0 @@
-class Person():
-
-    def __init__(self, name, last_name, email) -> None:
-        self.name = name
-        self.last_name = last_name
-        self.email = email
-
-    def toString(self):
-        print("Nome: {}\nSobrenome: {}\nEmail: {}".format(self.name, self.last_name, self.email))

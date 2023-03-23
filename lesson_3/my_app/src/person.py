9b8c3b7ea21a2469806cf9eb43a502d6eab5ca4d
caetanovns <caetanov120@gmail.com>
2023-03-23 19:55:09 -0300

    correções finalizadas

--- lesson_3/my_app/src/person.py
+++ lesson_3/my_app/src/person.py
@@ -1,9 +1,19 @@
+'''pacote person'''
+
+
 class Person():
+    """Classe person"""
 
     def __init__(self, name, last_name, email) -> None:
+        '''Construtor da classe'''
         self.name = name
         self.last_name = last_name
         self.email = email
 
-    def toString(self):
-        print("Nome: {}\nSobrenome: {}\nEmail: {}".format(self.name, self.last_name, self.email))
+    def to_string(self):
+        '''Método para retornar person em string'''
+        print(
+            f"Nome: {self.name}\nSobrenome: {self.last_name}\nEmail: {self.email}")
+
+    def pub2(self):
+        '''Novo metodo'''
